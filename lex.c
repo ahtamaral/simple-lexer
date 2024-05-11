06920e23a9d1ef3ac95365f97c4ef8e96aab930a
Tales Moreira <talescmoreira@gmail.com>
2024-05-10 21:20:13 -0300

    Update lex.c
    Completo rework

--- lex.c
+++ lex.c
@@ -1,74 +1,283 @@
-// Trabalho 01 Linguagens formais 2024.1
-
-// Artur Henrique Teixeira do Amaral
-// Tales Coutinho Moreira
-
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 
-// var a = 1 + 2
+#define MOVE_BACK_ONE_CHARACTER fseek(fd, -1, SEEK_CUR)
 
-#define MAX_TOKEN_SIZE 1024
+enum tokenType {
+    KEYWORD,
+    IDENTIFIER,
+    CHARACTER,
+    STRING,
+    SPECIAL_SYMBOL,
+    OPERADOR_ARITMETICO,
+    OPERADOR_LOGICO,
+    OPERADOR_RELACIONAL,
+    OPERADOR_DE_ATRIBUICAO,
+    OUTRO_OPERADOR,
+    NUMERO,
+    COMENTARIO,
+    FIM_DE_ARQUIVO,
+    TIPO_DE_DADO
+};
 
-typedef struct _charBuffer
-{
-    char cur;
-    char next;
-} charBuffer;
+char strType[14][100] = {
+    "PALAVRA_CHAVE",
+    "IDENTIFICADOR",
+    "CARACTERE",
+    "STRING",
+    "SIMBOLO_ESPECIAL",
+    "OPERADOR_ARITMETICO",
+    "OPERADOR_LOGICO",
+    "OPERADOR_RELACIONAL",
+    "OPERADOR_DE_ATRIBUICAO",
+    "OUTRO_OPERADOR",
+    "NUMERO",
+    "COMENTARIO",
+    "FIM_DE_ARQUIVO",
+    "TIPO_DE_DADO"
+};
 
-typedef struct _flags
-{
-    int isNumber;
-    int isName;
-    int isSymbol;
-    int isComment;
-} flags;
+char darr[11][30] = {
+    "char",
+    "double",
+    "enum",
+    "float",
+    "int",
+    "long",
+    "short",
+    "signed",
+    "struct",
+    "unsigned",
+    "void"
+};
 
-typedef char tokenBuffer[MAX_TOKEN_SIZE];
+typedef struct token {
+    char lex[100];
+    enum tokenType type;
+} token;
 
-typedef struct _lexer
-{
-    charBuffer charBuffer;
-    flags flags;
-    tokenBuffer tokenBuffer;
-} Lexer;
+FILE *fd;
 
-// Vão retornar flags que indicam o tipo que o token atual pode assumir, dada a leitura de seus caracteres.
-// int isNumber(charBuffer b)
-// int isName(charBuffer b)
-// int isComment(charBuffer b)
-// int isSymbol(charBuffer b)
-
-// int isReservedWord()
-// int isSpace()
-
-// Checa as flags do Lexer. Caso uma, e apenas uma, seja verdadeira, sabe-se inequivocamente qual é o tipo do token 
-// sendo lido no momento.
-int isCurrentTokenAmbiguous(flags flags)
-{
-    int ret = flags.isNumber + flags.isName + flags.isSymbol + flags.isComment;
-    return ret == 1 ? 1 : 0; 
+void preprocess(char *filename) {
+    FILE *fa = fopen(filename, "r");
+    if (!fa) {
+        printf("Erro ao abrir o arquivo\n");
+        exit(1);
+    }
+    fd = fa;
 }
 
+int isSpecialSymbol(int ca) {
+    return (ca == '[' || ca == ']' || ca == '(' || ca == ')' || ca == '{' || ca == '}' || ca == ',' || ca == ';' || ca == ':' || ca == '?');
+}
 
-int main()
-{
-    Lexer lexer;
+int isOperator(int ca) {
+    return (ca == '=' || ca == '+' || ca == '-' || ca == '*' || ca == '/' || ca == '%' || ca == '<' || ca == '>' || ca == '!' || ca == '&' || ca == '|' || ca == '^' || ca == '~' || ca == '.');
+}
 
-    lexer.charBuffer.cur = getchar();
+token getNextToken() {
+    char keywords[32][300] = { 
+        "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while" 
+    };
 
-    if (lexer.charBuffer.cur == EOF) { exit (1); }
+    int ca;
+    int j = 0;
+    token var;
 
-    while(1) {
+    ca = getc(fd);
+    memset(var.lex, 0, sizeof(var.lex));
+    
+    if (ca == EOF) {
+        var.type = FIM_DE_ARQUIVO;
+        return var;
+    }
 
-        lexer.charBuffer.next = getchar();
+    switch (ca) {
+        case '\n':
+            return getNextToken();
+        case '"':
+            j = 0;
+            while ((ca = getc(fd)) != '"' && ca != EOF) {
+                var.lex[j++] = ca;
+            }
+            var.type = STRING;
+            return var;
+        case '/':
+            ca = getc(fd);
+            if (ca == '/') {
+                j = 0;
+                var.type = COMENTARIO;
+                while ((ca = getc(fd)) != '\n' && ca != EOF) {
+                    var.lex[j++] = ca;
+                }
+                return var;
+            }
+            else if (ca == '*') {
+                j = 0;
+                var.type = COMENTARIO;
+                while (1) {
+                    var.lex[j++] = ca;
+                    while ((ca = getc(fd)) != '*') {
+                        if (ca == EOF) {
+                            printf("Erro: Comentário não terminado\n");
+                            exit(1);
+                        }
+                        var.lex[j++] = ca;
+                    }
+                    var.lex[j++] = ca;
+                    if ((ca = getc(fd)) == '/') {
+                        var.lex[j++] = ca;
+                        break;
+                    }
+                }
+                return var;
+            }
+            else {
+                var.type = SPECIAL_SYMBOL;
+                if (ca == '=') {
+                    var.type = OPERADOR_DE_ATRIBUICAO;
+                }
+                else {
+                    MOVE_BACK_ONE_CHARACTER;
+                }
+                var.lex[0] = '/';
+                return var;
+            }
+        case '\'':
+            var.lex[0] = ca;
+            if ((ca = getc(fd)) == '\\') {
+                var.lex[1] = ca;
+                ca = getc(fd);
+            }
+            var.lex[1] = ca;
+            ca = getc(fd);
+            var.type = CHARACTER;
+            return var;
+        default:
+            if (isdigit(ca)) {
+                j = 0;
+                while (!isSpecialSymbol(ca) && !isOperator(ca) && ca != ' ' && ca != '\n' && ca != '\t' && ca != '\r' && ca != EOF) {
+                    var.lex[j++] = ca;
+                    ca = getc(fd);
+                }
+                MOVE_BACK_ONE_CHARACTER;
+                var.type = NUMERO;
+                return var;
+            }
+            else if (isalpha(ca) || ca == '_') {
+                while (!isSpecialSymbol(ca) && !isOperator(ca) && ca != ' ' && ca != '\n' && ca != '\t' && ca != '\r' && ca != EOF) {
+                    var.lex[j++] = ca;
+                    ca = getc(fd);
+                }
+                MOVE_BACK_ONE_CHARACTER;
+                var.type = IDENTIFIER;
+                for (int i = 0; i < 32; i++) {
+                    if (strcmp(var.lex, keywords[i]) == 0) {
+                        var.type = KEYWORD;
+                        break;
+                    }
+                }
+                for (int j = 0; j < 11; j++) {
+                    if (strcmp(var.lex, darr[j]) == 0) {
+                        var.type = TIPO_DE_DADO;
+                        break;
+                    }
+                }
+                return var;                           
+            }
+            else {
+                var.lex[0] = ca;
+                switch (ca) {
+                    case '=':
+                    case '!':
+                        if ((ca = getc(fd)) == '=') {
+                            var.lex[j++] = ca;
+                            var.type = OPERADOR_RELACIONAL;
+                        }
+                        else {
+                            MOVE_BACK_ONE_CHARACTER;
+                            var.type = OPERADOR_ARITMETICO;
+                        }
+                        return var;
+                    case '<':
+                    case '>':
+                        if ((ca = getc(fd)) == '=') {
+                            var.lex[j++] = ca;
+                            var.type = OPERADOR_RELACIONAL;
+                        }
+                        else {
+                            MOVE_BACK_ONE_CHARACTER;
+                            var.type = OPERADOR_RELACIONAL;
+                        }
+                        return var;
+                    case '*':
+                    case '%':
+                        if ((ca = getc(fd)) == '=') {
+                            var.lex[j++] = ca;
+                            var.type = OPERADOR_DE_ATRIBUICAO;
+                        }
+                        else {
+                            MOVE_BACK_ONE_CHARACTER;
+                            var.type = OPERADOR_ARITMETICO;
+                        }
+                        return var;
+                    case '+':
+                    case '-':
+                        if ((ca = getc(fd)) == '=') {
+                            var.lex[j++] = ca;
+                            var.type = OPERADOR_DE_ATRIBUICAO;
+                        }
+                        else {
+                            MOVE_BACK_ONE_CHARACTER;
+                            var.type = OPERADOR_ARITMETICO;
+                        }
+                        return var;
+                    case '|':
+                    case '&':
+                        if ((ca = getc(fd)) == ca) {
+                            var.lex[j++] = ca;
+                            var.type = OPERADOR_LOGICO;
+                        }
+                        else {
+                            MOVE_BACK_ONE_CHARACTER;
+                            var.type = OPERADOR_ARITMETICO;
+                        }
+                        return var;
+                    case '^':
+                    case '~':
+                    case '.':
+                        var.type = OUTRO_OPERADOR;
+                        return var;
+                    default:
+                        return getNextToken();
+                }
+            }
+    }
+}
 
-        if (lexer.charBuffer.next == EOF ) { exit (1); }
+// Função principal
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Uso: %s <nome_do_arquivo>\n", argv[0]);
+        return 1;
+    }
 
-        printf("%c%c\n", lexer.charBuffer.cur, lexer.charBuffer.next);
+    // Pré-processamento do arquivo de entrada
+    preprocess(argv[1]);
 
-        lexer.charBuffer.cur = lexer.charBuffer.next;
-    }   
+    token temp;
+
+    printf("Token\t\tTipo\n");
+
+    // Obtém e imprime os tokens do arquivo de entrada até o final do arquivo
+    while ((temp = getNextToken()).type != FIM_DE_ARQUIVO) {
+        printf("%s\t%s\n", strType[temp.type], temp.lex);
+    }
+
+    // Fecha o arquivo de entrada
+    fclose(fd);
 
     return 0;
 }
